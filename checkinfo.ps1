f88442d32071f9c2d3784cac8d208583f3c5c6bf
drhoangzp <114561932+drhoangzp@users.noreply.github.com>
2023-12-29 21:48:55 +0700

    Update checkinfo.ps1

--- checkinfo.ps1
+++ checkinfo.ps1
@@ -5,9 +5,43 @@ $bios = Get-WmiObject -Class Win32_BIOS
 # Hiển thị thông tin hệ thống
 Write-Host "Serial Number: $($bios.SerialNumber)"
 Write-Host "Operating System: $($systemInfo.Caption)"
-Write-Host "Version: $($systemInfo.Version)"
-Write-Host "Build Number: $($systemInfo.BuildNumber)"
-Write-Host "Service Pack: $($systemInfo.ServicePackMajorVersion).$($systemInfo.ServicePackMinorVersion)"
-Write-Host "Installed RAM: $([math]::round($systemInfo.TotalVisibleMemorySize / 1MB, 2)) GB"
-Write-Host "Free Physical Memory: $([math]::round($systemInfo.FreePhysicalMemory / 1MB, 2)) GB"
-Write-Host "Free Virtual Memory: $([math]::round($systemInfo.FreeVirtualMemory / 1MB, 2)) GB"
+
+# Lấy thông tin hệ thống bằng Get-CimInstance
+$operatingSystem = Get-CimInstance Win32_OperatingSystem
+$computerSystem = Get-CimInstance Win32_ComputerSystem
+$physicalMemory = Get-CimInstance Win32_PhysicalMemory
+$diskDrive = Get-CimInstance Win32_DiskDrive
+$battery = Get-CimInstance Win32_Battery
+$processor = Get-CimInstance Win32_Processor
+
+# Hiển thị thông tin hệ thống
+Write-Host "Hostname: $($computerSystem.Name)"
+Write-Host "Model: $($computerSystem.Model)"
+Write-Host "Operating System: $($operatingSystem.Caption)"
+Write-Host "Version: $($operatingSystem.Version)"
+Write-Host "Build Number: $($operatingSystem.BuildNumber)"
+Write-Host "Service Pack: $($operatingSystem.ServicePackMajorVersion).$($operatingSystem.ServicePackMinorVersion)"
+Write-Host "Installed RAM: $([math]::round($physicalMemory.Capacity / 1GB, 2)) GB"
+
+# Lấy thông tin ổ cứng
+Write-Host "Disk Drive:"
+foreach ($drive in $diskDrive) {
+    Write-Host "  Model: $($drive.Model)"
+    Write-Host "  Type: $($drive.MediaType)"
+    Write-Host "  Capacity: $([math]::round($drive.Size / 1GB, 2)) GB"
+}
+
+# Lấy thông tin CPU
+Write-Host "Processor: $($processor.Name)"
+Write-Host "  Architecture: $($processor.Architecture)"
+Write-Host "  Cores: $($processor.NumberOfCores)"
+Write-Host "  Threads: $($processor.NumberOfLogicalProcessors)"
+
+# Lấy thông tin pin
+if ($battery) {
+    Write-Host "Battery:"
+    Write-Host "  Battery Status: $($battery.BatteryStatus)"
+    Write-Host "  Remaining Capacity: $($battery.EstimatedChargeRemaining)%"
+} else {
+    Write-Host "Battery information not available."
+}
